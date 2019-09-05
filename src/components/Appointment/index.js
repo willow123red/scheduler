42b428f237e0ce782baeb6379eff7c4d585b2f67
willow123red <ljwillow@hotmail.com>
2019-09-04 18:24:55 -0600

    add appointments exercises

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      <Header time={props.time}></Header>
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        ></Show>
+      ) : (
+        <Empty onAdd={props.onAdd}></Empty>
+      )}
+    </article>
+  );
+}
